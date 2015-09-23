3137e26e29dbb4f5d63a275ec8f8fa5eed9ac965
Eric Tsai <ekt7be@virginia.edu>
2015-09-23 16:22:05 -0400

    testing walls and changed method format of Seek, Flee and Arrive

--- Assets/Scripts/MovementBehavior.cs
+++ Assets/Scripts/MovementBehavior.cs
@@ -13,9 +13,10 @@ public class MovementBehavior : MonoBehaviour
 {
 	public GameObject targetObj;
 	public Behavior thisBehavior;
+
 	private float maxSpeed = 10f;
 	private float maxForce = 10f;
-	private Vector3 targetPos, currentPos, currentVelocity, finalSteering;
+	private Vector3 currentPos, currentVelocity, finalSteering, targetPosition;
 	private RaycastHit hitInfo;
 
 	void Start ()
@@ -25,19 +26,24 @@ public class MovementBehavior : MonoBehaviour
 	
 	void FixedUpdate ()
 	{
-		finalSteering = Vector3.zero;
-		targetPos = targetObj.transform.position;
+		targetPosition = targetObj.transform.position;
 		currentPos = this.transform.position;
+
+		finalSteering = Vector3.zero;
 		if (thisBehavior == Behavior.Seek)
-			finalSteering += Seek ();
-		else if (thisBehavior == Behavior.Flee)
-			finalSteering += Flee ();
-		else if (thisBehavior == Behavior.Arrive)
-			finalSteering += Arrive ();
+			finalSteering += Seek (targetPosition);
+		if (thisBehavior == Behavior.Flee)
+			finalSteering += Flee (targetPosition);
+		if (thisBehavior == Behavior.Arrive)
+			finalSteering += Arrive (targetPosition);
 		finalSteering += ObstacleAvoidance ();
+		finalSteering.y = 0f; //zero out y velocities
+
 		currentVelocity = Vector3.ClampMagnitude (currentVelocity + finalSteering * Time.deltaTime, maxSpeed);
 		this.transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.LookRotation (currentVelocity), Time.deltaTime);
 		this.transform.position = currentPos + currentVelocity * Time.deltaTime;
+
+		//change Zombie animation speed based on current speed
 		this.GetComponent<Animator> ().SetFloat ("currentSpeed", currentVelocity.magnitude);
 	}
 
@@ -48,9 +54,9 @@ public class MovementBehavior : MonoBehaviour
 		float detectDistance = minAheadDist + currentVelocity.magnitude / maxSpeed * minAheadDist;
 		//origin, direction, out var, maxDistance
 		Vector3 velocityAhead = currentVelocity.normalized * detectDistance;
-		Debug.DrawRay (currentPos, velocityAhead, Color.yellow);
 		string sphereTag = "SphereObs";
 		string wallTag = "WallObs";
+		Debug.DrawRay (currentPos, velocityAhead, Color.yellow);
 		if (Physics.Raycast (currentPos, velocityAhead, out hitInfo, detectDistance)) {
 			if (hitInfo.transform.tag == sphereTag) {
 				Vector3 collisionVelocity = hitInfo.transform.position - this.transform.position;
@@ -83,7 +89,7 @@ public class MovementBehavior : MonoBehaviour
 		return obsSteering * maxForce;
 	}
 
-	public Vector3 Seek ()
+	public Vector3 Seek (Vector3 targetPos)
 	{
 		Vector3 desiredVelocity = (targetPos - currentPos).normalized * maxSpeed;
 		Vector3 steering = desiredVelocity - currentVelocity;
@@ -94,7 +100,7 @@ public class MovementBehavior : MonoBehaviour
 		return steering;
 	}
 
-	public Vector3 Flee ()
+	public Vector3 Flee (Vector3 targetPos)
 	{
 		Vector3 desiredVelocity = (currentPos - targetPos).normalized * maxSpeed;
 		Vector3 steering = desiredVelocity - currentVelocity;
@@ -105,7 +111,7 @@ public class MovementBehavior : MonoBehaviour
 		return steering;
 	}
 
-	public Vector3 Arrive ()
+	public Vector3 Arrive (Vector3 targetPos)
 	{
 		float slowDistance = 20f;
 		float distance = (targetPos - currentPos).magnitude;

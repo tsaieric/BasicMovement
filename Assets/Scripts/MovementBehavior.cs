4d1c678958d86805a084d88bdea3b557c04f2dfd
Eric Tsai <ekt7be@virginia.edu>
2015-09-23 15:12:52 -0400

    sphere collisions work 90% of time. Flee and arrival should be working. Zombie anim based on currentSpeed

---
+++ Assets/Scripts/MovementBehavior.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Behavior
+{
+	Seek,
+	Flee,
+	Arrive,
+	Wander
+}
+
+public class MovementBehavior : MonoBehaviour
+{
+	public GameObject targetObj;
+	public Behavior thisBehavior;
+	private float maxSpeed = 10f;
+	private float maxForce = 10f;
+	private Vector3 targetPos, currentPos, currentVelocity, finalSteering;
+	private RaycastHit hitInfo;
+
+	void Start ()
+	{
+		currentVelocity = Vector3.zero;
+	}
+	
+	void FixedUpdate ()
+	{
+		finalSteering = Vector3.zero;
+		targetPos = targetObj.transform.position;
+		currentPos = this.transform.position;
+		if (thisBehavior == Behavior.Seek)
+			finalSteering += Seek ();
+		else if (thisBehavior == Behavior.Flee)
+			finalSteering += Flee ();
+		else if (thisBehavior == Behavior.Arrive)
+			finalSteering += Arrive ();
+		finalSteering += ObstacleAvoidance ();
+		currentVelocity = Vector3.ClampMagnitude (currentVelocity + finalSteering * Time.deltaTime, maxSpeed);
+		this.transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.LookRotation (currentVelocity), Time.deltaTime);
+		this.transform.position = currentPos + currentVelocity * Time.deltaTime;
+		this.GetComponent<Animator> ().SetFloat ("currentSpeed", currentVelocity.magnitude);
+	}
+
+	public Vector3 ObstacleAvoidance ()
+	{
+		Vector3 obsSteering = Vector3.zero;
+		float minAheadDist = 3f;
+		float detectDistance = minAheadDist + currentVelocity.magnitude / maxSpeed * minAheadDist;
+		//origin, direction, out var, maxDistance
+		Vector3 velocityAhead = currentVelocity.normalized * detectDistance;
+		Debug.DrawRay (currentPos, velocityAhead, Color.yellow);
+		string sphereTag = "SphereObs";
+		string wallTag = "WallObs";
+		if (Physics.Raycast (currentPos, velocityAhead, out hitInfo, detectDistance)) {
+			if (hitInfo.transform.tag == sphereTag) {
+				Vector3 collisionVelocity = hitInfo.transform.position - this.transform.position;
+				Vector3 steering = velocityAhead - collisionVelocity;
+				Debug.DrawRay (currentPos, steering * maxForce, Color.red);
+				obsSteering += steering;
+			}
+		}
+		velocityAhead = Quaternion.Euler (0, 45, 0) * velocityAhead;
+		Debug.DrawRay (currentPos, velocityAhead, Color.yellow);
+		if (Physics.Raycast (currentPos, velocityAhead, out hitInfo, detectDistance)) {
+			if (hitInfo.transform.tag == sphereTag) {
+				Vector3 collisionVelocity = hitInfo.transform.position - this.transform.position;
+				Vector3 steering = velocityAhead - collisionVelocity;
+				Debug.DrawRay (currentPos, steering * maxForce, Color.red);
+				obsSteering += steering;
+			}
+		}
+
+		velocityAhead = Quaternion.Euler (0, -90, 0) * velocityAhead;//rotating +45-90 = -45 degrees
+		Debug.DrawRay (currentPos, velocityAhead, Color.yellow);
+		if (Physics.Raycast (currentPos, velocityAhead, out hitInfo, detectDistance)) {
+			if (hitInfo.transform.tag == sphereTag) {
+				Vector3 collisionVelocity = hitInfo.transform.position - this.transform.position;
+				Vector3 steering = velocityAhead - collisionVelocity;
+				Debug.DrawRay (currentPos, steering * maxForce, Color.red);
+				obsSteering += steering;
+			} 
+		}
+		return obsSteering * maxForce;
+	}
+
+	public Vector3 Seek ()
+	{
+		Vector3 desiredVelocity = (targetPos - currentPos).normalized * maxSpeed;
+		Vector3 steering = desiredVelocity - currentVelocity;
+		if (steering.magnitude < 1f)
+			steering = steering.normalized;
+		else
+			steering = Vector3.ClampMagnitude (steering, maxForce);
+		return steering;
+	}
+
+	public Vector3 Flee ()
+	{
+		Vector3 desiredVelocity = (currentPos - targetPos).normalized * maxSpeed;
+		Vector3 steering = desiredVelocity - currentVelocity;
+		if (steering.magnitude < 1f)
+			steering = steering.normalized;
+		else
+			steering = Vector3.ClampMagnitude (steering, maxForce);
+		return steering;
+	}
+
+	public Vector3 Arrive ()
+	{
+		float slowDistance = 20f;
+		float distance = (targetPos - currentPos).magnitude;
+		float slowSpeed = Mathf.Clamp (maxSpeed * distance / slowDistance, 0f, maxSpeed);
+		Vector3 desiredVelocity = (targetPos - currentPos).normalized * slowSpeed;
+		Vector3 steering = desiredVelocity - currentVelocity;
+		if (steering.magnitude < 1f)
+			steering = steering.normalized;
+		else
+			steering = Vector3.ClampMagnitude (steering, maxForce);
+		return steering;
+	}
+
+	public void Wander ()
+	{
+
+	}
+}
---
+++ Assets/Scripts/TargetHop.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetHop : MonoBehaviour
+{
+	public float changePosDelay = 3.5f;
+	public Vector3[]
+		positions = new Vector3[4];
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine (ChangePosition ());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+
+	public IEnumerator ChangePosition ()
+	{
+		yield return new WaitForSeconds (changePosDelay);
+		int index = Random.Range (0, positions.Length - 1);
+		this.transform.position = positions [index];
+		StartCoroutine (ChangePosition ());
+	}
+}

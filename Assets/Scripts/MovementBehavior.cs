4b63b467bdc5171735334852be11a514a9212959
Eric Tsai <ericktsai@cmu.edu>
2015-09-24 12:43:21 -0400

    reduced redundancy for obstacle avoidance method, created helper method for it

--- Assets/Scripts/MovementBehavior.cs
+++ Assets/Scripts/MovementBehavior.cs
@@ -18,6 +18,8 @@ public class MovementBehavior : MonoBehaviour
 	private float maxForce = 30f;
 	private float randomAngle = 0f;
 	private Vector3 currentPos, currentVelocity, finalSteering, targetPosition;
+	private string sphereTag = "SphereObs";
+	private string wallTag = "WallObs";
 	private RaycastHit hitInfo;
 
 	void Start ()
@@ -36,9 +38,9 @@ public class MovementBehavior : MonoBehaviour
 		if (thisBehavior == Behavior.Flee)
 			finalSteering += Flee (targetPosition);
 		if (thisBehavior == Behavior.Arrive)
-			finalSteering += Arrive (targetPosition);
+			finalSteering += Arrive (targetPosition, 20f);
 		if (thisBehavior == Behavior.Wander)
-			finalSteering += Wander ();
+			finalSteering += Wander (15f, 120f) * 2f;
 		finalSteering += ObstacleAvoidance () * 20f;
 		finalSteering.y = 0f; //zero out y velocities
 		finalSteering = Vector3.ClampMagnitude (finalSteering, maxForce);
@@ -69,60 +71,40 @@ public class MovementBehavior : MonoBehaviour
 		float detectDistance = minAheadDist + currentVelocity.magnitude / maxSpeed * minAheadDist;
 
 		//Raycast ahead
-		Vector3 velocityAhead = currentVelocity.normalized * detectDistance;
-		Debug.DrawRay (currentPos, velocityAhead.normalized * detectDistance, Color.yellow);
-		if (Physics.Raycast (currentPos, velocityAhead, out hitInfo, detectDistance)) {
-			if (hitInfo.transform.tag == sphereTag) {
-				Vector3 collisionVelocity = hitInfo.transform.position - this.transform.position;
-				Vector3 steering = velocityAhead - collisionVelocity;
-				Debug.DrawRay (currentPos, steering * maxForce, Color.red);
-				obsSteering += steering;
-			}
-			if (hitInfo.transform.tag == wallTag) {
-				float lengthPastWall = ((currentPos + velocityAhead.normalized * detectDistance) - hitInfo.point).magnitude;
-				Vector3 steering = hitInfo.normal.normalized * lengthPastWall;
-				Debug.DrawRay (currentPos, steering, Color.red);
-				obsSteering += steering;
-			} 
-		}
-		
-		//Raycast right
-		velocityAhead = Quaternion.Euler (0, 45, 0) * velocityAhead;
-		Debug.DrawRay (currentPos, velocityAhead.normalized * detectDistance, Color.yellow);
-		if (Physics.Raycast (currentPos, velocityAhead, out hitInfo, detectDistance)) {
-			if (hitInfo.transform.tag == sphereTag) {
-				Vector3 collisionVelocity = hitInfo.transform.position - this.transform.position;
-				Vector3 steering = velocityAhead - collisionVelocity;
-				Debug.DrawRay (currentPos, steering * maxForce, Color.red);
-				obsSteering += steering;
-			}
-			if (hitInfo.transform.tag == wallTag) {
-				float lengthPastWall = ((currentPos + velocityAhead.normalized * detectDistance) - hitInfo.point).magnitude;
-				Vector3 steering = hitInfo.normal.normalized * lengthPastWall;
-				Debug.DrawRay (currentPos, steering, Color.red);
-				obsSteering += steering;
-			} 
-		}
-		
-		//Raycast left
-		velocityAhead = Quaternion.Euler (0, -90, 0) * velocityAhead;//rotating +45-90 = -45 degrees
-		Debug.DrawRay (currentPos, velocityAhead.normalized * detectDistance, Color.yellow);
-		if (Physics.Raycast (currentPos, velocityAhead, out hitInfo, detectDistance)) {
-			if (hitInfo.transform.tag == sphereTag) {
-				Vector3 collisionVelocity = hitInfo.transform.position - this.transform.position;
-				Vector3 steering = velocityAhead - collisionVelocity;
-				Debug.DrawRay (currentPos, steering, Color.red);
-				obsSteering += steering;
-			} 
+		Vector3 raycastVector = currentVelocity.normalized * detectDistance;
+		obsSteering += ObstacleHelperMethod (raycastVector, detectDistance);
 
-			if (hitInfo.transform.tag == wallTag) {
-				float lengthPastWall = ((currentPos + velocityAhead.normalized * detectDistance) - hitInfo.point).magnitude;
-				Vector3 steering = hitInfo.normal.normalized * lengthPastWall;
+		//Raycast right
+		raycastVector = Quaternion.Euler (0, 45, 0) * raycastVector;
+		obsSteering += ObstacleHelperMethod (raycastVector, detectDistance);
+
+		//Raycast left
+		raycastVector = Quaternion.Euler (0, -90, 0) * raycastVector;//rotating +45-90 = -45 degrees
+		obsSteering += ObstacleHelperMethod (raycastVector, detectDistance);
+
+		return obsSteering;
+	}
+
+	private Vector3 ObstacleHelperMethod (Vector3 raycastVector, float detectDistance)
+	{
+		Vector3 combinedSteering = Vector3.zero;
+		Debug.DrawRay (currentPos, raycastVector.normalized * detectDistance, Color.yellow);
+		if (Physics.Raycast (currentPos, raycastVector, out hitInfo, detectDistance)) {
+			if (hitInfo.transform.tag == sphereTag) {
+				Vector3 collisionVelocity = hitInfo.transform.position - this.transform.position;
+				Vector3 steering = raycastVector - collisionVelocity;
 				Debug.DrawRay (currentPos, steering, Color.red);
-				obsSteering += steering;
+				combinedSteering += steering;
+			} 
+			
+			if (hitInfo.transform.tag == wallTag) {
+				float lengthPastWall = ((currentPos + raycastVector.normalized * detectDistance) - hitInfo.point).magnitude;
+				Vector3 steering = hitInfo.normal.normalized * lengthPastWall;
+				Debug.DrawRay (currentPos, steering, Color.black);
+				combinedSteering += steering;
 			} 
 		}
-		return obsSteering;
+		return combinedSteering;
 	}
 
 	public Vector3 Seek (Vector3 targetPos)
@@ -147,9 +129,8 @@ public class MovementBehavior : MonoBehaviour
 		return steering;
 	}
 
-	public Vector3 Arrive (Vector3 targetPos)
+	public Vector3 Arrive (Vector3 targetPos, float slowDistance)
 	{
-		float slowDistance = 20f;
 		float distance = (targetPos - currentPos).magnitude;
 		//distance/slowDistance will be greater than 1 if he's farther away
 		//slowSpeed will never exceed maxSpeed because of clamp.
@@ -163,11 +144,11 @@ public class MovementBehavior : MonoBehaviour
 		return steering;
 	}
 
-	public Vector3 Wander ()
+	public Vector3 Wander (float angleRange, float maxAngle)
 	{
-		randomAngle = randomAngle + Random.Range (-15f, 15f);
-		//reset randomAngle if it goes past 120,-120 degrees
-		if (Mathf.Abs (randomAngle) > 120f)
+		randomAngle = randomAngle + Random.Range (-angleRange, angleRange);
+		//reset randomAngle if it goes past maxAngle degrees
+		if (Mathf.Abs (randomAngle) > maxAngle)
 			randomAngle = 0f;
 		Vector3 desiredVelocity = Quaternion.Euler (0, randomAngle, 0) * currentVelocity;
 		if (currentVelocity == Vector3.zero)

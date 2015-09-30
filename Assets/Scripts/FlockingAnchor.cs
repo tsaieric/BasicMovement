d1ff99f7da4468bccb2a408136e7c9c577f2ac25
chengqixi <chengqixi@gmail.com>
2015-09-30 13:31:54 -0400

    vshape kind of working
    array index is out of range

--- Assets/Scripts/FlockingAnchor.cs
+++ Assets/Scripts/FlockingAnchor.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public enum Formation
 {
     Circle,
+	Vshape,
     Triangle
 }
 public class FlockingAnchor : MonoBehaviour {
@@ -12,7 +13,7 @@ public class FlockingAnchor : MonoBehaviour {
     public Formation form;
     private MoveController[] flockObjects;
     private int numObjects;
-    private Vector3 avgCurrentVelocity;
+    //private Vector3 avgCurrentVelocity;
     private Vector3 avgCenterPos;
     private float radius = 15f;
     private MoveController movement;
@@ -35,7 +36,7 @@ public class FlockingAnchor : MonoBehaviour {
             totalCurrentVelocity += obj.GetCurrentVelocity();
             totalCenterPos += obj.transform.position;
         }
-        avgCurrentVelocity = totalCurrentVelocity / numObjects;
+        //avgCurrentVelocity = totalCurrentVelocity / numObjects;
         avgCenterPos = totalCenterPos / numObjects;
 
         switch(this.form)
@@ -56,6 +57,37 @@ public class FlockingAnchor : MonoBehaviour {
                     obj.ObstacleAvoidance(2f, 20f);
                     obj.UpdateEverything();
                     count++;
+                }
+                break;
+			case Formation.Vshape:
+                Vector3 front = Vector3.forward.normalized;
+                Vector3 currentPos;
+				float interval = radius/4;
+                //int count = 0;
+                //float rotateBy = 360 / numObjects;
+                avgCenterPos = this.transform.position;
+				for (int i=0; i<= numObjects/2-1; i++)
+				{	MoveController obj = flockObjects[i];
+                    Vector3 left = Quaternion.Euler(0f, -135f, 0f) * front;
+                    Debug.DrawRay(avgCenterPos, left * radius, Color.black);
+                    currentPos = avgCenterPos + left * interval;
+                    obj.Arrive(currentPos, 10f); //arrive to their position
+                    obj.Separation(5f, 10f);
+                    obj.ObstacleAvoidance(2f, 20f);
+                    obj.UpdateEverything();
+					avgCenterPos = currentPos;
+                }
+				avgCenterPos = this.transform.position;
+				for (int i=numObjects/2; i<numObjects; i++)
+				{	MoveController obj = flockObjects[i];
+                    Vector3 right = Quaternion.Euler(0f, 135f, 0f) * front;
+                    Debug.DrawRay(avgCenterPos, right * radius, Color.black);
+                    currentPos = avgCenterPos + right * interval;
+                    obj.Arrive(currentPos, 10f); //arrive to their position
+                    obj.Separation(5f, 10f);
+                    obj.ObstacleAvoidance(2f, 20f);
+                    obj.UpdateEverything();
+					avgCenterPos = currentPos;
                 }
                 break;
             case Formation.Triangle:

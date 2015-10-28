8165c03e308319fa407f8cecf3f00145aab7a420
Eric Tsai <ericktsai@cmu.edu>
2015-10-28 16:37:31 -0400

    added moving obstacles and Max animation has been fixed

---
+++ Assets/Scenes/PatrolArea.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PatrolArea : MonoBehaviour
+{
+	public Transform destA;
+	public float timeToDest;
+	private Vector3 origPos;
+	// Use this for initialization
+	void Start ()
+	{
+		origPos = this.transform.position;
+		StartCoroutine (MoveTo (origPos, destA.position, timeToDest));
+	}
+
+	IEnumerator MoveTo (Vector3 startPos, Vector3 desiredPos, float time)
+	{
+		float distance = Mathf.Abs (transform.position.magnitude - desiredPos.magnitude);
+		float moveSpeed = distance / time;
+		while (transform.position!=desiredPos) {
+			transform.position = Vector3.MoveTowards (transform.position, desiredPos, moveSpeed * Time.deltaTime);
+			yield return null;
+		}
+		StartCoroutine (MoveTo (desiredPos, startPos, time));
+		yield return null;
+	}
+}

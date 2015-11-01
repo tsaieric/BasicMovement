bc2543aa87ce79ebfb901445497718f7c934e06d
unknown <ericktsai@cmu.edu>
2015-11-01 16:17:03 -0500

    set the rotation of max to face mouse

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour
 {
 	public float speed = 6f;
-
+    public LayerMask floorMask;
+    float camRayLength = 150f;
 	Vector3 movement;
 	Animator anim;
 	Rigidbody playerRigidbody;
@@ -24,17 +25,29 @@ public class PlayerMovement : MonoBehaviour
 		float v = Input.GetAxis ("Vertical");
 
 		Move (h, v);
-		//this.transform.position += new Vector3(h, 0, v) * Time.deltaTime * speed;        
-		Vector3 direction = new Vector3 (h, 0, v);
-		if (direction != Vector3.zero)
-			this.transform.rotation = Quaternion.LookRotation (direction);
+        //this.transform.position += new Vector3(h, 0, v) * Time.deltaTime * speed;        
+        //Vector3 direction = new Vector3 (h, 0, v);
+        //if (direction != Vector3.zero)
+        //	this.transform.rotation = Quaternion.LookRotation (direction);
+        TurnToMouse();
 		Animating (h, v);
 	}
 
+    void TurnToMouse()
+    {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit rayHitInfo;
+        if(Physics.Raycast(mouseRay, out rayHitInfo, camRayLength,floorMask))
+        {
+            Vector3 playerToMouse = rayHitInfo.point - this.transform.position;
+            playerToMouse.y = 0f;
+            playerRigidbody.MoveRotation(Quaternion.LookRotation(playerToMouse));
+        }
+    }
 
 	void Move (float h, float v)
 	{
-		movement.Set (h, 0f, v);
+        movement.Set (h, 0f, v);
 		movement = movement.normalized * speed * Time.deltaTime;
 		playerRigidbody.MovePosition (transform.position + movement);
 

d0bee9a94d3d727b941579665ce9384433571939
unknown <ericktsai@cmu.edu>
2015-11-02 03:43:26 -0500

    added radar sending functionality, need to add radar feedback

---
+++ Assets/Scripts/PlayerActions.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerActions : MonoBehaviour
+{
+    public float radarSpeed = 10f;
+    private PlayerMovement movement;
+    private GameObject radar;
+    // Use this for initialization
+    void Start()
+    {
+        movement = this.GetComponent<PlayerMovement>();
+        radar = GameObject.FindGameObjectWithTag("Radar");
+        radar.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RadarControl();
+    }
+
+    void RadarControl()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            radar.SetActive(true);
+            movement.disabled = true;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Vector3 curScale = radar.transform.localScale;
+            radar.transform.localScale = Vector3.MoveTowards(curScale, curScale * 100, Time.deltaTime * radarSpeed);
+            radar.transform.Rotate(Vector3.up, 1f);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            StartCoroutine(ShrinkRadar());
+        }
+    }
+    IEnumerator ShrinkRadar()
+    {
+        while(radar.transform.localScale!=Vector3.one)
+        {
+            radar.transform.Rotate(Vector3.up, -1f);
+            Vector3 curScale = radar.transform.localScale;
+            radar.transform.localScale = Vector3.MoveTowards(curScale, Vector3.one, Time.deltaTime * 100f);
+            yield return null;
+        }
+        radar.SetActive(false);
+        movement.disabled = false;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,41 +3,49 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
-	public float speed = 6f;
+    public bool disabled = false;
+    public float speed = 6f;
     public LayerMask floorMask;
     float camRayLength = 1000f;
-	Vector3 movement;
-	Animator anim;
-	Rigidbody playerRigidbody;
-	// Use this for initialization
-	void Start ()
-	{
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidbody;
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        anim.SetFloat("currentSpeed", speed);
+    }
 
-		anim = GetComponent<Animator> ();
-		playerRigidbody = GetComponent<Rigidbody> ();
-		anim.SetFloat ("currentSpeed", speed);
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		float h = Input.GetAxis ("Horizontal");
-		float v = Input.GetAxis ("Vertical");
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
 
-		Move (h, v);
-        //this.transform.position += new Vector3(h, 0, v) * Time.deltaTime * speed;        
-        //Vector3 direction = new Vector3 (h, 0, v);
-        //if (direction != Vector3.zero)
-        //	this.transform.rotation = Quaternion.LookRotation (direction);
+        if (!disabled)
+        {
+            Move(h, v);
+            //this.transform.position += new Vector3(h, 0, v) * Time.deltaTime * speed;        
+            //Vector3 direction = new Vector3 (h, 0, v);
+            //if (direction != Vector3.zero)
+            //	this.transform.rotation = Quaternion.LookRotation (direction);
+        }
+        else
+        {
+            v = 0f;
+            h = 0f;
+        }
+        Animating(h, v);
         TurnToMouse();
-		Animating (h, v);
-	}
+    }
 
     void TurnToMouse()
     {
         Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit rayHitInfo;
-        if(Physics.Raycast(mouseRay, out rayHitInfo, camRayLength,floorMask))
+        if (Physics.Raycast(mouseRay, out rayHitInfo, camRayLength, floorMask))
         {
             Vector3 playerToMouse = rayHitInfo.point - this.transform.position;
             playerToMouse.y = 0f;
@@ -45,23 +53,23 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-	void Move (float h, float v)
-	{
-        movement.Set (h, 0f, v);
-		movement = movement.normalized * speed * Time.deltaTime;
-		playerRigidbody.MovePosition (transform.position + movement);
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
 
-	}
+    }
 
-	void Animating (float h, float v)
-	{
-		bool walking = h != 0f || v != 0f;
-		// Debug.Log(walking);
-		anim.SetBool ("IsWalking", walking);
+    void Animating(float h, float v)
+    {
+        bool walking = h != 0f || v != 0f;
+        // Debug.Log(walking);
+        anim.SetBool("IsWalking", walking);
         Vector3 moveDir = new Vector3(h, 0, v);
         //animate in reverse if you're moving backwards or forwards
         float result = Vector3.Dot(moveDir, this.transform.forward);
-        if (result>=0)
+        if (result >= 0)
         {
             anim.SetFloat("currentSpeed", speed);
         }

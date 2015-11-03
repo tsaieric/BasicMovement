cead47070a0b07545db7a704d2bf3be80efbedfe
Eric Tsai <ericktsai@cmu.edu>
2015-11-02 19:24:43 -0500

    added zombie attack logic, when they're within a dist of 2 from player

--- Assets/Scripts/ZombieScripts/ZombieActions.cs
+++ Assets/Scripts/ZombieScripts/ZombieActions.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ZombieActions : MonoBehaviour {
-
+public class ZombieActions : MonoBehaviour
+{
+	private Transform playerT;
+	private float attackDist;
+	private Animator anim;
+	void Awake ()
+	{
+		playerT = GameObject.FindGameObjectWithTag ("Player").transform;		
+		anim = this.GetComponent<Animator> ();
+	}
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		attackDist = 2f;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		float distanceFromPlayer = Vector3.Distance (playerT.position, this.transform.position);
+		if (distanceFromPlayer <= attackDist && !anim.GetBool ("isAttacking")) {
+			anim.SetBool ("isAttacking", true);
+		} else {
+			anim.SetBool ("isAttacking", false);
+		}
 	}
 }

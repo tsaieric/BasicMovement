459c05708bb9bc7bc82132cbda9701912700f57b
chengqixi <chengqixi@gmail.com>
2015-11-02 21:33:18 -0500

    add arrow, shooting script

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour
+{
+
+    private float bulletSpeed = 1000f;
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(this.gameObject, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Vector3.forward * bulletSpeed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        //Destroy (col.gameObject);
+        if (col.collider.gameObject.name == "Cube")
+        {
+            Debug.Log("collision");
+            Destroy(col.collider.gameObject);
+        }
+    }
+}
+

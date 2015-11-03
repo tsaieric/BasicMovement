cedecb53a06fbcc1c742bbf1a274785c54434fce
chengqixi <chengqixi@gmail.com>
2015-11-02 22:44:37 -0500

    add shotobullet
    shootbullet tiffa

---
+++ Assets/Scripts/PlayerScripts/ShootBullet.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootBullet : MonoBehaviour {
+    public float bulletSpeed = 10;
+    public Rigidbody bullet;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+            Fire();
+
+    }
+    void Fire()
+    {
+        Rigidbody bulletClone = (Rigidbody)Instantiate(bullet, transform.position, transform.rotation);
+        bulletClone.velocity = transform.forward * bulletSpeed;
+    }
+}

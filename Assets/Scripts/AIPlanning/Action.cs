0b13bebeefd3775796eed07f0c36a802909ca6a3
unknown <ericktsai@cmu.edu>
2015-11-25 16:00:34 -0500

    added actions and goalplanner basic basic version

---
+++ Assets/Scripts/AIPlanning/Action.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class Action : MonoBehaviour
+{
+
+    public Dictionary<string, object> preconditions;
+    public Dictionary<string, object> effects;
+    public float cost;
+
+    public Action()
+    {
+        preconditions = new Dictionary<string, object>();
+        effects = new Dictionary<string, object>();
+    }
+
+    public void AddPrecondition(string key, object value)
+    {
+        preconditions.Add(key, value);
+    }
+
+    public void RemovePrecondition(string key)
+    {
+        preconditions.Remove(key);
+    }
+
+    public void AddEffect(string key, object value)
+    {
+        effects.Add(key, value);
+    }
+
+    public void RemoveEffect(string key)
+    {
+        effects.Remove(key);
+    }
+
+    public virtual void Reset() { }
+}
---
+++ Assets/Scripts/AIPlanning/GoalPlanner.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlanNode
+{
+    public PlanNode parent;
+    public float totalCost;
+    public Dictionary<string, object> currentState;
+    public Action action;
+    public PlanNode(PlanNode parent, float cost, Dictionary<string, object> state, Action a)
+    {
+        this.parent = parent;
+        totalCost = cost;
+        currentState = state;
+        action = a;
+    }
+}
+
+public class GoalPlanner : MonoBehaviour
+{
+    public Queue<Action> Plan(HashSet<Action> availableActions, Dictionary<string,object> currentState, Dictionary<string,object> goalState) 
+    {
+        foreach(Action a in availableActions)
+        {
+            a.Reset();
+        }
+        List<PlanNode> leaves = new List<PlanNode>();
+        PlanNode leastCostLeaf = null;
+        PlanNode first = new PlanNode(null, 0f, currentState, null);
+        bool planAvailable = BuildTree(first, availableActions, goalState, leastCostLeaf);
+        if(planAvailable)
+        {
+            LinkedList<Action> list = new LinkedList<Action>();
+            while(leastCostLeaf!=null) {
+                list.AddFirst(leastCostLeaf.action);
+                leastCostLeaf = leastCostLeaf.parent;
+            }
+            return new Queue<Action>(list);
+        } else
+        {
+            Debug.Log("no plan available currently");
+            return null;
+        }
+    }
+
+    public bool BuildTree(PlanNode parent, HashSet<Action> actionList, Dictionary<string, object> goalState, List<PlanNode> leaves)
+    {
+        bool hasPath = false;
+        foreach (Action a in actionList)
+        {
+            if (AreConditionsInState(a.preconditions, parent.currentState))
+            {
+                Dictionary<string, object> newState = CreateStateWithEffects(a.effects, parent.currentState);
+                PlanNode newNode = new PlanNode(parent, parent.totalCost + a.cost, newState, a);
+                if (AreConditionsInState(goalState, newState))
+                {
+                    leaves.Add(newNode);
+                    hasPath = true;
+                }
+                else
+                {
+                    HashSet<Action> actionsMinusOne = new HashSet<Action>(actionList);
+                    actionsMinusOne.Remove(a);
+                    BuildTree(newNode, actionsMinusOne, goalState, leaves);
+                }
+            }
+        }
+        return hasPath;
+    }
+
+    //build tree and track the leastcostleaf node;
+    public bool BuildTree(PlanNode parent, HashSet<Action> actionList, Dictionary<string, object> goalState, PlanNode leastCostLeaf)
+    {
+        bool hasPath = false;
+        foreach (Action a in actionList)
+        {
+            if (AreConditionsInState(a.preconditions, parent.currentState))
+            {
+                Dictionary<string, object> newState = CreateStateWithEffects(a.effects, parent.currentState);
+                PlanNode newNode = new PlanNode(parent, parent.totalCost + a.cost, newState, a);
+                if (AreConditionsInState(goalState, newState))
+                {
+                    if (leastCostLeaf == null)
+                    {
+                        leastCostLeaf = newNode;
+                    }
+                    else if (newNode.totalCost < leastCostLeaf.totalCost)
+                    {
+                        leastCostLeaf = newNode;
+                    }
+                    hasPath = true;
+                }
+                else
+                {
+                    //remove the current action from list of available actions and build tree based on remaining actions
+                    HashSet<Action> actionsMinusOne = new HashSet<Action>(actionList);
+                    actionsMinusOne.Remove(a);
+                    //if any path was found, set to true
+                    hasPath = hasPath || BuildTree(newNode, actionsMinusOne, goalState, leastCostLeaf);
+                }
+            }
+        }
+        return hasPath;
+    }
+
+    private Dictionary<string, object> CreateStateWithEffects(Dictionary<string, object> effects, Dictionary<string, object> state)
+    {
+        Dictionary<string, object> result = new Dictionary<string, object>(state);
+        foreach (string key in effects.Keys)
+        {
+            result.Add(key, effects[key]);
+        }
+        return result;
+    }
+
+    private bool AreConditionsInState(Dictionary<string, object> conditions, Dictionary<string, object> state)
+    {
+        bool inState = true;
+        foreach (string key in conditions.Keys)
+        {
+            if (!state.ContainsKey(key))
+            {
+                return false;
+            }
+            else
+            {
+                if (!state[key].Equals(conditions[key]))
+                {
+                    return false;
+                }
+            }
+        }
+        return inState;
+    }
+}

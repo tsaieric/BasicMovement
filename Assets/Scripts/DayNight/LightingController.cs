df81dfc102cf39354a7334dee6f76e962999b79d
Eric Tsai <ericktsai@cmu.edu>
2015-12-09 13:37:01 -0500

    trying to tell when its day or night

--- Assets/Scripts/DayNight/LightingController.cs
+++ Assets/Scripts/DayNight/LightingController.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 public class LightingController : MonoBehaviour
 {
+	private bool isDay = true;
 	public float speed;
-
+	private float totalChange = 0f;
 	// Update is called once per frame
 	void Update ()
 	{
+		totalChange += Time.deltaTime * speed;
+		if (Mathf.Abs (totalChange - 3f) <= .01f) {
+			totalChange = 0;
+			isDay = !isDay;
+			Debug.Log (isDay);
+		}
 		this.transform.RotateAround (-Vector3.forward, 1f * Time.deltaTime * speed);
 	}
 }

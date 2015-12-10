53806f511e6ba6bea99a8c3b83edfb70d8edeec0
chengqixi <chengqixi@gmail.com>
2015-12-10 13:57:18 -0500

    soundmanager

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -23,5 +23,44 @@ public class SoundManager : Singleton<SoundManager> {
     {
         aSource.PlayOneShot(clips[7]);
     }
-
+	public void PlayerHurt()
+	{	
+		aSource.PlayOneShot(clip[10]);
+	}
+	public void PlayerDie()
+	{	
+		aSource.PlayOneShot (clip [9]);
+	}
+	public void PlayRadar()
+	{	
+		aSource.PlayOneShot (clip [12]);
+	}
+	public void DogAttack()
+	{
+		aSource.PlayOneShot (clip [4]);
+	}
+	public void DogHurt()
+	{
+		aSource.PlayOneShot (clip [6]);
+	}
+	public void ZombieAttack()
+	{
+		aSource.PlayOneShot (clip [13]);
+	}
+	public void ZombieDie()
+	{
+		aSource.PlayOneShot (clip [14]);
+	}
+	public void BigZombieRoar()
+	{
+		aSource.PlayOneShot (clip [1]);
+	}
+	public void NightCome()
+	{
+		aSource.PlayOneShot (clip [11]);
+	}
+	public void DayCome()
+	{
+		aSource.PlayOneShot (clip [3]);
+	}
 }

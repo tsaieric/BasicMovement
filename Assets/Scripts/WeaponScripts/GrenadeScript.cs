5d840bd5a0db3918ff9b235edd732af1cb9c34f9
Eric Tsai <ericktsai@cmu.edu>
2015-12-09 23:58:04 -0500

    expanded A* pathfinding grid, got explosions to kill multiple enemies and moved beacon a little

--- Assets/Scripts/WeaponScripts/GrenadeScript.cs
+++ Assets/Scripts/WeaponScripts/GrenadeScript.cs
@@ -3,19 +3,32 @@ using System.Collections;
 
 public class GrenadeScript : MonoBehaviour
 {
+	private EnemyHealth[] enemyHealths;
 	private float timeBeforeExplosion = 1.5f;
 	// Use this for initialization
 	void Start ()
 	{
+		enemyHealths = FindObjectsOfType<EnemyHealth> ();
 		Destroy (this.gameObject, timeBeforeExplosion);
 		StartCoroutine (Explode (timeBeforeExplosion));
 	}
 
 	IEnumerator Explode (float delay)
 	{
-		yield return new WaitForSeconds (delay - .2f);
+		yield return new WaitForSeconds (delay - .1f);
 		GameObject explosionParticles = (GameObject)Instantiate (Resources.Load ("GrenadeExplosion"), this.transform.position, Quaternion.identity);
 		Destroy (explosionParticles, 2f);
+		DamageNearestEnemies ();
+	}
 
+	void DamageNearestEnemies ()
+	{
+		float range = 8f;
+		foreach (EnemyHealth enemy in enemyHealths) {
+			Debug.Log (Vector3.Distance (enemy.transform.position, this.transform.position));
+			if (Vector3.Distance (enemy.transform.position, this.transform.position) < range) {
+				enemy.ReduceHealth (40f);
+			}
+		}
 	}
 }
